021e0947a837b443dd079e244a54ed395339236a
Klemens David Morgenstern <klemens.morgenstern@gmx.net>
2019-01-24 18:16:01 +0800

    Broke unity

--- test/unity_test.c
+++ test/unity_test.c
@@ -9,7 +9,7 @@ void test_AverageThreeBytes_should_AverageMidRangeValues(void)
 {
     TEST_ASSERT_EQUAL_HEX8(40, AverageThreeBytes(30, 40, 50));
     TEST_ASSERT_EQUAL_HEX8(40, AverageThreeBytes(10, 70, 40));
-    TEST_ASSERT_EQUAL_HEX8(33, AverageThreeBytes(33, 33, 33));
+    TEST_ASSERT_EQUAL_HEX8(34, AverageThreeBytes(33, 33, 33));
 }
 
 void test_AverageThreeBytes_should_AverageHighValues(void)
@@ -18,7 +18,7 @@ void test_AverageThreeBytes_should_AverageHighValues(void)
     TEST_ASSERT_EQUAL_HEX8(127, AverageThreeBytes(127, 127, 127));
     TEST_ASSERT_EQUAL_HEX8(84, AverageThreeBytes(0, 126, 126));
 
-    TEST_ASSERT_MESSAGE(1, "foo\nbar");
+    TEST_ASSERT_MESSAGE(0, "foo\nbar");
 }
 
 int main(void)
